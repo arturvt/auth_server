24934c812171e94ca5e195480d0c22477447e730
Artur Tenorio <atenorio@motorola.com>
2016-02-11 14:26:47 -0300

    All methods implemented...

---
+++ gemt/data/schema.sql
@@ -0,0 +1,8 @@
+drop table if exists auth_keys;
+create table auth_keys (
+  id integer primary key autoincrement,
+  reader_key text not null,
+  machine_id text,
+  created_date text not null,
+  authenticated_date text
+);
