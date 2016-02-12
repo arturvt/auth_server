151412204a9805864cff4f362eea5c6231ed4c27
Artur Tenorio <atenorio@motorola.com>
2016-02-12 19:42:32 -0300

    config heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn run_server:app --chdir gemt --log-file -
---
+++ gemt/runserver.py
@@ -0,0 +1,10 @@
+"""
+Run GEMT Server
+"""
+from gemt import app
+
+# app.run(debug=True)
+if __name__ == '__main__':
+    # app.logger.info('Starting app...')
+    app.run(debug=True)
+    # app.run()
